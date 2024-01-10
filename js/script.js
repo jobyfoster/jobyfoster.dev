052a8045dff6a5dae989b24e9788d471c55ef6d2
joby foster <jobyjfoster@gmail.com>
2024-01-09 23:14:31 -0600

    small baby changes

--- js/script.js
+++ js/script.js
@@ -27,32 +27,32 @@ tl.from(".nav-container", { y: -150, duration: 0.5, ease: 'power3.out' });
 
 // Introduction section animation
 tl.addLabel("introduction")
-  .from(".introduction", { y: -100, duration: 0.5, ease: "power2.in" }, "introduction")
-  .to(".introduction", { opacity: 1, duration: 0.3, ease: "power2.in" }, "<0.3")
-  .from(".hr", { width: "0%", duration: 0.75, ease: "power4.out" })
+  .from(".introduction", { y: -100, duration: 0.5, ease: "power2.inOut" }, "introduction")
+  .to(".introduction", { opacity: 1, duration: 0.35, ease: "power2.inOut" }, "<0.2")
+  .from(".hr", { width: "0%", duration: 0.65, ease: "power4.out" }, "<0.2")
 
 // Background text animation
-tl.addLabel("backgroundText", ">-0.5")
-  .from(".background-text", { x: -800, duration: 1, ease: "power4.out", stagger: 0.2 }, "backgroundText");
+tl.addLabel("backgroundText", ">-0.3")
+  .from(".background-text", { x: -800, duration: 0.5, ease: "power4.out", stagger: 0.2 }, "backgroundText");
 
 // Skills section animation
-tl.addLabel("skills", "-=1")
-  .from(".skills-container", { x: 300, opacity: 0 }, "skills")
-  .from(".skill", { y: 250, duration: 0.5, stagger: 0.05, ease: "power4.in" }, "<")
-
+tl.addLabel("skills", ">0.2")
+  .from(".skills-container", { x: 300, duration: 0.5, opacity: 0 }, "skills")
+  .from(".skill", { y: 250, duration: 0.5, stagger: 0.05, ease: "power4.inOut" }, "<0.1")
 
 // Resume section animation
-tl.addLabel("resume", "-=1.1")
-  .from("#resume", { x: -400, opacity: 0, ease: "power4.inOut"}, "skills")
+tl.addLabel("resume")
+  .from("#resume", { x: -400, opacity: 0, ease: "power4.inOut", duration: 0.5 }, "skills+=0.3")
 
 // Projects heading animation
-tl.addLabel("projects")
-  .from("#eduflip-card", {x: -500, opacity: 0, duration: 0.5, ease: 'power2.in'}, ">-0.5")
-  .from("#snuscheck-card", {x: 500, opacity: 0, duration: 0.5, ease: 'power2.in'}, ">-0.5")
+tl.addLabel("projects", ">0.3")
+  .from("#eduflip-card", {x: -500, opacity: 0, duration: 0.5, ease: 'power2.inOut'}, "<0.4")
+  .from("#snuscheck-card", {x: 500, opacity: 0, duration: 0.5, ease: 'power2.inOut'}, "<0.4")
 
 // Hand animation
 gsap.set(".hand", {x: 72, y: -45, scaleX: -1})
 tl.to(".hand", { rotation: 25, duration: 0.25, ease: "power3.inOut", yoyo: true, repeat: 9 }, "<1");
+
 
 
 const lenis = new Lenis()
